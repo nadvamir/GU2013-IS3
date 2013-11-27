d16f8b27383c455c7f4505329cc3a403ab77a3a2
Maksim Solovjov <himaxx2@gmail.com>
2013-11-27 20:02:15 +0000

    Initial commit, create the skeleton
    There are four html files, each for one of the four screens.
    Subscreens are implemented as article elements within the main screen.
    There is only basic CSS available, style your own pages if you like,
    but keep them consistent.

---
+++ controls.js
@@ -0,0 +1,22 @@
+function toggleMainNav() {
+	$('#left ol').slideToggle();
+}
+
+function openSubwindow( id ) {
+	if ($(this).hasClass( 'active' ))
+		return;
+	$('#left ul li.active').removeClass( 'active' );
+	$(this).addClass( 'active' );
+	// these were cool, you can uncomment these and
+	// comment the rest, but I got tired of them quickly
+	//$('.subwindow').fadeOut(200);
+	//$('#'+id).fadeIn(200);
+	$('.subwindow').hide();
+	$('#'+id).show();
+}
+
+// on the load of the document
+$(document).ready( function() {
+	$('.subwindow').first().show();
+	$('#left ul li').first().addClass('active');
+});
