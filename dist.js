32de13e94cbd777b194555669628d64fb606e564
elxnis <reinis@elksnis.lv>
2013-12-01 16:40:12 +0000

    distribution selection

--- dist.js
+++ dist.js
@@ -1,26 +1,35 @@
-function dchart() {
-  google.load("visualization", "1", {packages:["corechart"]});
-  google.setOnLoadCallback(drawChart);
-  function drawChart() {
-	var data = google.visualization.arrayToDataTable(dataset);
+function select() {
+	console.log(1);
+	var mylist = document.getElementById("distList");
+	var title = mylist.options[mylist.selectedIndex].text;
+	var dataset = [
+		['Range', 'Countries'],
+		['1',  660],
+		['2',  1000],
+		['3',  1030],
+		['4',  1170],
+		['5',  1500],
+		['6',  1300],
+		['7',  1030],
+		['8',  850],
+		['9',  730],
+		['10',  600]
+	];
+	console.log(2);
+	google.load("visualization", "1", {packages:["corechart"], "callback" : drawChart});
+	function drawChart() {
+		console.log(3);
+		var data = google.visualization.arrayToDataTable(dataset);
+		var options = {
+			title: title,
+			width: 0,
+			height: 440
+		};
 
-	var options = {
-	  title: title,
-	  width: 800,
-	  height: 500
+		var distchart = new google.visualization.LineChart(document.getElementById('dist_chart'));
+		distchart.draw(data, options);
 	};
+	console.log(4);
+};
 
-	var distchart = new google.visualization.LineChart(document.getElementById('dist_chart'));
-	distchart.draw(data, options);
-  }
-}
 
-var dataset = [
-	['Country', 'datadatadata'],
-	['LV',  660],
-	['USA',  1000],
-	['LT',  1030],
-	['UK',  1170]	  
-];
-
-var title = 'Count per county';
