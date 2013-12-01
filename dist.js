0e371ccf4ebc40c6d0c593abd23158817c76dfbe
elxnis <reinis@elksnis.lv>
2013-12-01 13:31:06 +0000

    sample graph

---
+++ dist.js
@@ -0,0 +1,24 @@
+function dchart() {
+  google.load("visualization", "1", {packages:["corechart"]});
+  google.setOnLoadCallback(drawChart);
+  function drawChart() {
+	var data = google.visualization.arrayToDataTable(dataset);
+
+	var options = {
+	  title: title
+	};
+
+	var distchart = new google.visualization.LineChart(document.getElementById('dist_chart'));
+	distchart.draw(data, options);
+  }
+}
+
+var dataset = [
+	['Country', 'datadatadata'],
+	['LV',  660],
+	['USA',  1000],
+	['LT',  1030],
+	['UK',  1170]	  
+];
+
+var title = 'Count per county';
