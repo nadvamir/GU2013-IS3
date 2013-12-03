6933e4a16106c7229d6757d73ce1945307c59245
Maksim Solovjov <himaxx2@gmail.com>
2013-12-03 19:13:43 +0000

    Add 2D visualisation of the information
    Previously clustering, but there are no clear clusters in the data, it is more
    or less firmly distributed with bizarre outliers, usually consisting of the USA,
    India, China, Brazil and Russia. In any case, the plots do provide interesting
    insigths, so we might as well keep them.

---
+++ clusters.js
@@ -0,0 +1,43 @@
+google.load("visualization", "1", {packages:["corechart"]});
+
+function getData( dataArray ) {
+    var data = new google.visualization.DataTable();
+    data.addColumn( 'number', '1st dimension' );
+    data.addColumn( 'number', '2nd dimension' );
+    data.addColumn({ type: 'string', role: 'tooltip' });
+    data.addRows( dataArray );
+    return data;
+}
+
+function getOptions() {
+    return {
+        title: '2D projection of the multidimensional dataset',
+        hAxis: {title: '1st Dimension'},
+        vAxis: {title: '2st Dimension'},
+        legend: 'none'
+    };
+}
+
+function drawFullDataset() {
+    var data = getData( pcaFullDataset );
+    var options = getOptions();
+
+    var chart = new google.visualization.ScatterChart( $('#full-dataset-plot')[0] );
+    chart.draw( data, options );
+}
+
+function drawWelfareDataset() {
+    var data = getData( pcaWelfareDataset );
+    var options = getOptions();
+
+    var chart = new google.visualization.ScatterChart( $('#welfare-dataset-plot')[0] );
+    chart.draw( data, options );
+}
+
+function drawHealthcareDataset() {
+    var data = getData( pcaHealtcareDataset );
+    var options = getOptions();
+
+    var chart = new google.visualization.ScatterChart( $('#healthcare-dataset-plot')[0] );
+    chart.draw( data, options );
+}
